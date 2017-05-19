e83fbb344e694ec8c4cd9e06543a4f77512603c9
xiaoxuisaac <xiaoxuisaac@gmail.com>
2017-05-19 02:22:26 -0400

    add preference admin

--- gargoyle/admin.py
+++ gargoyle/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('day','name')
 
+
 class TempFileAdmin(admin.ModelAdmin):
     list_display = ('upload','owner','saved_date')  
+
+
+class VocabPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('owner','show_cutoff','diff_cutoff','learner_pref','webster_pref')  
+
 
 class SessionVariablesAdmin(admin.ModelAdmin):
     list_display = ('request_id','saved_date','owner')  
 
 admin.site.register(Comment,CommentAdmin)
 admin.site.register(TempFile,TempFileAdmin)
+admin.site.register(VocabPreference,VocabPreferenceAdmin)
 admin.site.register(SessionVariables,SessionVariablesAdmin)
 
