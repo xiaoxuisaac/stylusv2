d8f7850bd4c211a13977f8bd3a8b1db02d6f9525
xiaoxuisaac <xiaoxuisaac@gmail.com>
2017-05-19 03:52:52 -0400

    turn off update undefined

--- stylusv2/settings.py
+++ stylusv2/settings.py
@@ -89,7 +89,7 @@ JENKINS_TASKS = (
 WSGI_APPLICATION = 'stylusv2.wsgi.application'
 
 # lexicon Setting
-UNDEFINED_UPDATE = True
+UNDEFINED_UPDATE = False
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
